ea9c2e21a0cd080b8d239c7d9e89816be69af17c
Nguyễn Văn Tiến <50447619+NguyenTien110@users.noreply.github.com>
2023-03-08 11:46:03 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,5 +2,7 @@ module.exports = {
   reactStrictMode: false,
   images: {
     domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com'],
+    loader: 'akamai',
+    path: ''
   },
 }
